1b8491b7951abd0b49e13fd3c4c500a7c3839344
Rymunin <chrisbrhodes@hotmail.com>
2020-08-10 18:18:30 +0100

    added travis deploy edited files

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          final run possibly
+          hello to all of Rymunin's friends
         </p>
         <a
           className="App-link"
